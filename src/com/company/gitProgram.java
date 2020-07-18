f74db34e838a8d293c05f83819f13da3c83bf8d4
Sagar-Pant <sagarpant56@gmail.com>
2020-07-18 20:55:32 +0530

    Second Commit

---
+++ src/com/company/gitProgram.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class gitProgram {
+
+    public static void main(String[] args) {
+    System.out.println("This is a simple application of git concept!!!");
+     }
+}
