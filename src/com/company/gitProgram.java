8d2b1c444e307c86ea38fbf1be0cc5584c0ab0e1
Sagar-Pant <sagarpant56@gmail.com>
2020-07-18 22:00:23 +0530

    Asian Commit

--- src/com/company/gitProgram.java
+++ src/com/company/gitProgram.java
@@ -1,8 +1,8 @@
 package com.company;
-
 public class gitProgram {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
     System.out.println("This is a simple application of git concept!!!");
-     }
+        System.out.println("This is a fun learning simple application of git concept!!!");
+    }
 }
