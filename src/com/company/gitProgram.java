a31ae8ed7acd3d5bc24c2d084ed522a58118dc33
Sagar-Pant <sagarpant56@gmail.com>
2020-07-18 22:21:01 +0530

    American Commit

--- src/com/company/gitProgram.java
+++ src/com/company/gitProgram.java
@@ -4,5 +4,6 @@ public class gitProgram {
     {
     System.out.println("This is a simple application of git concept!!!");
         System.out.println("This is a fun learning simple application of git concept!!!");
+        System.out.println("This is a complete fun learning process!!!Believe Me!!!");
     }
 }
